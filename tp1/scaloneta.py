b4a0440c2fa44dfcdfc0eec9425e270e733ea307
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2023-09-09 16:42:01 -0300

    Create scaloneta.py

---
+++ tp1/scaloneta.py
@@ -0,0 +1,35 @@
+import sys
+
+class Analysis:
+    def __init__(self, si, ai):
+        self.si = si # coach analysis duration
+        self.ai = ai # helper analysis duration
+
+    def __str__(self):
+        return str(self.si) + ", " + str(self.ai)
+
+def readAnalysisFile(filename):
+    analysis_list = []
+    with open(filename, "r") as file:
+        for line in file:
+            si, ai = line.split(",")
+            
+            if not si.strip().isdigit() or not ai.strip().isdigit():
+                continue 
+            
+            analysis = Analysis(int(si), int(ai))
+            analysis_list.append(analysis)
+
+    return analysis_list
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("not enough arguments")
+        return
+    
+    analysis_list = readAnalysisFile(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
