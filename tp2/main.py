45abab3ed6389c7d18be0363f80f9a9ffada27b8
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2023-10-04 16:28:30 -0300

    Add algorithm to determine training schedule

--- tp2/main.py
+++ tp2/main.py
@@ -26,6 +26,8 @@ def parse_input(file_name):
 def g_max(n, e, s):
     g = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 
+    max_g = 0
+
     for day in range(1, n + 1):
         for cons_trainings in range(day + 1):
             g[day][0] = max(g[day][0], g[day - 1][cons_trainings])
@@ -33,7 +35,35 @@ def g_max(n, e, s):
                 training_earning = min(e[day - 1], s[cons_trainings - 1])
                 g[day][cons_trainings] = g[day - 1][cons_trainings - 1] + training_earning
 
-    return max(g[n])
+                if g[day][cons_trainings] > max_g:
+                    max_g = g[day][cons_trainings]
+
+    return g, max_g
+
+def max_index(row):
+    max = row[0]
+    max_idx = 0
+    for i in range(len(row)):
+        if row[i] > max:
+            max = row[i]
+            max_idx = i
+    
+    return max_idx
+
+def show_trained_days(g, n):
+    i = n
+    while i > 0:
+        j = max_index(g[i])
+
+        for t in range(j):
+            print("Dia " + str(i) + ": Entrenamiento")
+            i -= 1
+
+        if i == 0:
+            break 
+
+        print("Dia " + str(i) + ": Descanso")
+        i = i -1
 
 
 if __name__ == '__main__':
@@ -41,5 +71,7 @@ if __name__ == '__main__':
         print("Uso: python3 main.py <archivo>")
         exit(1)
     n, e, s = parse_input(argv[1])
-    print("La ganancia máxima es:", g_max(n, e, s))
+    g, max_g = g_max(n, e ,s)
+    print("La ganancia máxima es:", max_g)
+    show_trained_days(g, n)
 
